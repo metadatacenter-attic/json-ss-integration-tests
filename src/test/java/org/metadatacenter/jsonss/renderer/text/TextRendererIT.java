1ec295c13d67890d6c1edf1ca8cd32fd356e9f85
martin <martin@zippyrate.com>
2015-09-07 19:14:27 -0700

    Some integration test infrastructure work

--- src/test/java/org/metadatacenter/jsonss/renderer/text/TextRendererIT.java
+++ src/test/java/org/metadatacenter/jsonss/renderer/text/TextRendererIT.java
@@ -9,6 +9,9 @@ import org.metadatacenter.jsonss.parser.ParseException;
 import org.metadatacenter.jsonss.test.IntegrationTestBase;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class TextRendererIT extends IntegrationTestBase
 {
@@ -25,5 +28,13 @@ public class TextRendererIT extends IntegrationTestBase
   @Test public void TestTest()
       throws ParseException, IOException
   {
+    Label cellA1 = createCell("BMW", 1, 1);
+    Set<Label> cells = createCells(cellA1);
+    Map<String, Set<Label>> ss = new HashMap<>();
+    ss.put(SHEET1, cells);
+
+    String expression = "{ \"hasCar\": @A1 }";
+//    Optional<TextRendering> rendering = createTextRendering(ss, expression, settings);
+//    assertThat(rendering.isPresent(), is(true));
   }
 }
